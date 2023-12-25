5d43b9fee3a69a1c198b728bab218e8da576cdf7
Marcelo Hadad <marcelo.hadad@outlook.com>
2023-12-25 17:29:04 -0300

    feat: Add function to convert PokeAPI Pokemon details to the PokemonProfile class

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -17,6 +17,29 @@ function convertPokeApiDetailToPokemon(pokeApiDetail) {
     return pokemon
 }
 
+function convertPokeApiDetailToPokemonProfile(pokeApiDetail) {
+    const pokemonProfile = new PokemonProfile()
+    
+    const types = pokeApiDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+    const stats = pokeApiDetail.stats.map((statSlot) => (statSlot.base_stat))
+    const [hp, attack, defense, speed] = stats
+
+    pokemonProfile.id = pokeApiDetail.id
+    pokemonProfile.name = pokeApiDetail.name
+    pokemonProfile.hp = hp
+    pokemonProfile.attack = attack
+    pokemonProfile.defense = defense
+    pokemonProfile.speed = speed
+    pokemonProfile.types = types
+    pokemonProfile.type = type
+    pokemonProfile.weight = pokeApiDetail.weight
+    pokemonProfile.height = pokeApiDetail.height
+    pokemonProfile.photo = pokeApiDetail.sprites.other.dream_world.front_default
+    
+    return pokemonProfile
+}
+
 pokeApi.getPokemonDetail = (pokemon) => {
     return fetch(pokemon.url)
             .then((response) => response.json())
